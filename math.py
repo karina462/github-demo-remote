c4ce450313cda7e26e3f70418cdf48cde116516d
karinagodhwani <karinagodhwani52@gmail.com>
2021-08-02 14:35:22 +0530

    added fn headers

--- math.py
+++ math.py
@@ -1,9 +1,12 @@
+# add implementation
 def add(x,y):
     pass
     
+# subtract implementation
 def subtract(x,y):
     pass
-    
+
+# multiply implementation    
 def multiply(x,y):
     pass
     
